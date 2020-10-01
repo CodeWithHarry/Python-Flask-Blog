218e45385ca4fcbea18548321348a27c17593c32
Sanket Agarwal <agarwalanil2801@gmail.com>
2020-10-01 16:33:22 +0530

    changed some code to one liners in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug import secure_filename
+# from werkzeug import secure_filename
 from flask_mail import Mail
 import json
 import os
@@ -8,8 +8,8 @@ import math
 from datetime import datetime
 
 
-with open('config.json', 'r') as c:
-    params = json.load(c)["params"]
+
+params = json.load(open("./config.json", "r"))['params']
 
 local_server = True
 app = Flask(__name__)
