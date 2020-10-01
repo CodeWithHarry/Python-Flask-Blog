da9741abc3cc5eaf0943f23408a50d42a4719bca
Sanket Agarwal <agarwalanil2801@gmail.com>
2020-10-01 16:25:25 +0530

    increased size of blog from 120 to 1000

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class Posts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     slug = db.Column(db.String(21), nullable=False)
-    content = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
     tagline = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable=True)
     img_file = db.Column(db.String(12), nullable=True)
